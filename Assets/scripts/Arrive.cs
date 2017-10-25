61c49bbe764a00e59d3f6ab9127322fa42da7fb5
pawcijo <apanapantesz@o2.pl>
2017-10-26 01:40:31 +0200

    2 new scripts

---
+++ Assets/scripts/Arrive.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrive : AgentBehavior {
+
+    public float targetRadius;
+    public float slowRadius;
+    public float timeToTarget = 0.1f;
+
+
+
+    public override Steering GetSteering()
+    {
+        Steering steering = new Steering();
+        Vector3 direction = target.transform.position - transform.position;
+        float distance = direction.magnitude;
+        float targetSpeed;
+
+        if(distance <targetRadius)
+        {
+            return steering;
+        }
+        if(distance >slowRadius)
+        {
+            targetSpeed = agent.maxSpeed;
+
+        }
+        else
+        {
+            targetSpeed = agent.maxSpeed * distance / slowRadius;
+        }
+
+        // ---------------desiredVelocity-------------------//
+
+        Vector3 desiredVelocity = direction;
+        desiredVelocity.Normalize();
+        desiredVelocity *= targetSpeed;
+        steering.linerar = desiredVelocity - agent.velocity;
+        steering.linerar /= timeToTarget;
+
+        if(steering.linerar.magnitude>agent.maxSpeed)
+        {
+            steering.linerar.Normalize();
+            steering.linerar *= agent.maxSpeed;
+        }
+
+        return steering;
+
+    }
+
+
+
+}
---
+++ Assets/scripts/Leave.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Leave : AgentBehavior
+{
+    public float escapeRadius;
+    public float dangerRadius;
+    public float timetoTarget = 0.1f;
+
+    //Steering steering = new Steering();
+    //Vector3 direction = transform.p
+
+
+    public override Steering GetSteering()
+    {
+
+        Steering steering = new Steering();
+        Vector3 direction = transform.position - target.transform.position;
+        float distance = direction.magnitude;
+
+        if(distance>dangerRadius)
+        {
+            return steering;
+        }
+        float reduce;
+        if(distance <escapeRadius)
+        {
+            reduce = 0.0f;
+        }
+        else
+        {
+            reduce = distance / dangerRadius * agent.maxSpeed;
+        }
+        float targetSpeed = agent.maxSpeed - reduce;
+
+        //---------------
+
+        Vector3 desiredVelocity = direction;
+        desiredVelocity.Normalize();
+        desiredVelocity *= targetSpeed;
+        steering.linerar = desiredVelocity - agent.velocity;
+        steering.linerar /= timetoTarget;
+
+        if (steering.linerar.magnitude > agent.maxSpeed)
+        {
+            steering.linerar.Normalize();
+            steering.linerar *= agent.maxSpeed;
+        }
+
+        return steering;
+
+    }
+}
